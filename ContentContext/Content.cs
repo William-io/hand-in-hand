c7efa4255bfbb99c95f7b0c0705760cf954cf003
William <capuletos@live.com>
2021-05-30 13:10:24 -0300

    I created the attributes in the Content class that will be the initial inheritance base.

---
+++ ContentContext/Content.cs
@@ -0,0 +1,12 @@
+namespace ContentContext
+{
+    /*Classe base para herança*/
+    class Content
+    {
+        public int Id { get; set; }
+        public string Title {get; set;}
+        public string Url { get; set; }
+
+    }
+    
+}
