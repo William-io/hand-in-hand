f0b94ec45dfa2102c6cd44b432e31d8c41c2c668
William <capuletos@live.com>
2021-06-04 12:03:07 -0300

    Simulating course entry

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Hand.ContentContext;
 
 namespace Hand
@@ -8,6 +9,18 @@ namespace Hand
         static void Main(string[] args)
         {
             Console.Clear();
+            var articles = new List<Article>();
+            //Suponha que o usuario colocou as informações do seu livro. para iniciar a progressão.
+            articles.Add(new Article("Algoritmos Estruturas, Harry Farrer", "Programação Estruturada"));
+            articles.Add(new Article("Programando para processadores paralelos, Kirk Kwu", "Programação GPU"));
+            articles.Add(new Article("Enciclopédia da Nuvem, Luli Radfharer", "Conceitos & atualidades"));
+
+            foreach (var article in articles)
+            {
+                System.Console.WriteLine(article.Id);
+                System.Console.WriteLine(article.Title);
+                System.Console.WriteLine(article.Url);
+            }
             
         }
     }
